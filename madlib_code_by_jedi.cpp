24e87577c4df33d7f171bf89dbd3735557daba1f
Jedika Hardeman <99002683+Jedika@users.noreply.github.com>
2022-02-03 21:48:20 -0800

    Add files via upload

---
+++ madlib_code_by_jedi.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+string adjective, adjective2, adjective3, number, liquid, adjective4, Toothpaste, adjective5, adjective6, adjective7, noun, adjective8, pluralNoun, color, noun2, noun3;
+
+
+    cout << "enter an adjective: ";
+     getline (cin, adjective) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective2) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective3) ;
+    cout << "enter a number: ";
+     getline (cin, number) ;
+    cout << "enter a liquid: ";
+     getline (cin, liquid) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective4) ;
+    cout << "enter the name of a toothpaste: ";
+     getline (cin, Toothpaste) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective5) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective6) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective7) ;
+    cout << "enter a noun: ";
+     getline (cin, noun) ;
+    cout << "enter an adjective: ";
+     getline (cin, adjective8) ;
+    cout << "enter a plural noun: ";
+     getline (cin, pluralNoun) ;
+    cout << "enter a color: ";
+     getline (cin, color) ;
+    cout << "enter a noun: ";
+     getline (cin, noun2) ;
+    cout << "enter a noun: ";
+     getline (cin, noun3) ;
+
+    cout << "A Commercial Message From The Sponcer" << endl;
+    cout << " " << endl;
+    cout << "Friends, have you noticed that your teeth are starting to look " << adjective << " and " << adjective2 << "?" << endl;
+    cout << "That's because you've been using the wrong toothpaste. Chomp" << endl;
+    cout << "Toothpaste will make your teeth " << adjective3 << " aftter only" << endl;
+    cout << number << " brushings. That's because Chomp Toothpaste" << endl;
+    cout << "contains Hex-a-chlor-a-" << liquid << ", a secret ingredient" << endl;
+    cout << "known to your " << adjective4 << " druggist as " << Toothpaste << "." << endl;
+    cout << "Chomp attacks the " << adjective5 << " acids in your mouth and" << endl;
+    cout << "leaves your breath " << adjective6 << " and " << adjective7 << ". It will" << endl;
+    cout << "make your " << noun << " feel " << adjective8 << " and will also" << endl;
+    cout << "stimulate your " << pluralNoun << ". Always keep the familiar" << endl;
+    cout << color << " tube of Chomp handy in your " << noun2 << "." << endl;
+    cout << "And now, back to our western program, Have " << noun3 << "," << endl;
+    cout << "Will Travel!" << endl;
+
+    return 0;
+}
